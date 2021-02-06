83bde55216674ecfe8b9aacb4f3033b219a21677
Kyle <kyledbrownell@gmail.com>
2021-02-06 12:42:52 -0500

    Added support for LCD info panel

--- trackingSolarPanels.cs
+++ trackingSolarPanels.cs
@@ -1,4 +1,4 @@
-const float spd = 0.05f;
+const float spd = 0.1f;
 const float delPwr = 5;
 
 const int HOMING = 0;
@@ -18,40 +18,57 @@ public Program () {
 public void Main (string argument, UpdateType updateSource) {
     var h1 = (IMyMotorAdvancedStator)GridTerminalSystem.GetBlockWithName("Hinge 1");
     var h2 = (IMyMotorAdvancedStator)GridTerminalSystem.GetBlockWithName("Hinge 2");
-    List<IMyTerminalBlock> panels = new List<IMyTerminalBlock>();
-    GridTerminalSystem.SearchBlocksOfName("Solar Panel", panels);
     IMyInteriorLight light =  (IMyInteriorLight)GridTerminalSystem.GetBlockWithName("Panel Light");
+    List<IMyTerminalBlock> panels = new List<IMyTerminalBlock>();
+    List<IMyTerminalBlock> LCDs = new List<IMyTerminalBlock>();
+    GridTerminalSystem.SearchBlocksOfName("Solar Panel", panels);
+    GridTerminalSystem.SearchBlocksOfName("LCD Panel", LCDs);
+    float pwr = 0;
+    string message = "";
 
-    float pwr = 0; 
+
+    //========= GET TOTAL PANEL POWER =========//
     foreach (IMySolarPanel panel in panels) {
         pwr += GetPanelPower(panel);
     }
+    //========================================//
 
+
+    //============  PROCESS STATE ============//
     if (pwr < 0.01) state = HOMING;
     if (state == HOMING) Home(h1, h2, light);
     if (state == WAITING) Wait(h2, pwr, light);
     if (state == TRACKY || state == TRACKX) Track(h1, h2, pwr, light);
-    
-    Echo ("Pitch: " + (h1.Angle).ToString());
-    Echo ("Yaw: " + (h2.Angle).ToString());
-    Echo ("Power: " + pwr.ToString());
-    Echo ("");
-    Echo (GetState());
-    if (state == WAITING) {
-        Echo ("Power change: " + Math.Abs(pwr - pwr0).ToString());
-        Echo ("Power change threshold: " + delPwr.ToString());
-    }
+    if (state != WAITING) pwr0 = pwr;  // set pwr0 for next step
+    //======================================//
 
-    if (state != WAITING) pwr0 = pwr;
+
+    //==== WRITE MESSAGE TO CONSOLE, LCDS ====//
+    message += "Pitch: " + (h1.Angle).ToString() + "\n";
+    message += "Yaw: " + (h2.Angle).ToString() + "\n";
+    message += "Power: " + pwr.ToString() + "\n\n";
+    message += GetState() + "\n";
+    if (state == WAITING) {
+        message += "Power change: " + Math.Abs(pwr - pwr0).ToString() + "\n";
+        message += "Power change threshold: " + delPwr.ToString() + "\n";
+    }
+    foreach (IMyTextPanel LCD in LCDs) {
+        LCD.WritePublicText(message, false);  // append=false
+        LCD.ShowPublicTextOnScreen();
+    }
+    Echo (message);
+    //======================================//
+
+
 }
 
-public float GetPanelPower (IMySolarPanel panel) { 
-	    string[] detail = (panel.DetailedInfo.Split('\n',' '));
-	    return Convert.ToSingle(detail[5]);
-} 
+public float GetPanelPower (IMySolarPanel panel) {
+            string[] detail = (panel.DetailedInfo.Split('\n',' '));
+            return Convert.ToSingle(detail[5]);
+}
 
 void Home (IMyMotorAdvancedStator h1, IMyMotorAdvancedStator h2, IMyInteriorLight light) {
-    light.SetValue<Color>( "Color",  new Color(255, 0, 0) ); 
+    light.SetValue<Color>( "Color",  new Color(255, 0, 0) );
     if (h1.Angle < 1) h1.TargetVelocityRPM = 1;
     else h1.TargetVelocityRPM = 0;
     if (h2.Angle > -1.2) h2.TargetVelocityRPM = -1;
@@ -60,7 +77,7 @@ void Home (IMyMotorAdvancedStator h1, IMyMotorAdvancedStator h2, IMyInteriorLigh
 }
 
 void Wait (IMyMotorAdvancedStator h2, float pwr, IMyInteriorLight light) {
-    light.SetValue<Color>( "Color",  new Color(255, 255, 0) ); 
+    light.SetValue<Color>( "Color",  new Color(255, 255, 0) );
     if (Math.Abs(pwr0 - pwr) > delPwr) {
         state = TRACKX;
         h2.TargetVelocityRPM = spd*xDir;
@@ -70,8 +87,8 @@ void Wait (IMyMotorAdvancedStator h2, float pwr, IMyInteriorLight light) {
 }
 
 void Track (IMyMotorAdvancedStator h1, IMyMotorAdvancedStator h2, float pwr, IMyInteriorLight light) {
-    light.SetValue<Color>( "Color",  new Color(0, 255, 0) ); 
-    if (pwr < pwr0) { 
+    light.SetValue<Color>( "Color",  new Color(0, 255, 0) );
+    if (pwr < pwr0) {
         if (state == TRACKX) {
             h2.TargetVelocityRPM = 0;
             state = TRACKY;
